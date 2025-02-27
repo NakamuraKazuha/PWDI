33af495fb08d3fa1ca24259f2d41013c8bfe74b2
Ileryadeh <ipdy@up.edu.ph>
2025-02-27 22:11:55 +0800

    Create commands.py
    function for each button

---
+++ commands.py
@@ -0,0 +1,14 @@
+def emergency_button(e):
+    print("Emergency Button!")
+
+def share_button(e):
+    print("Share")
+
+def home_button(e):
+    print("Home Button")
+
+def profile(e):
+    print("Profile")
+
+def notif(e):
+    print("Notification")
