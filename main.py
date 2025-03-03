09ab2c83d828b65d928c5f40d70f96d1c3809a85
Ileryadeh <ipdy@up.edu.ph>
2025-03-03 23:50:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
+
 from flet import *
-from commands import emergency_button, share_button, home_button, profile, notif
 from first_interface import create_ui
 from flet import app
 from splash import splash_screen
 
+from profile_page import create_profile_ui
 
-
-def main(page: Page):
-  def main(page: Page):
+import os
+def main(page: Page):  
     page.window.width = 440
     page.window.height = 956
     page.window.resizable = False
 
-    page.clean() 
-    page.add(create_ui(page))  
+    page.fonts = {
+        "lale": "fonts/Lalezar-Regular.ttf",
+        "Chiller": "fonts/chiller.ttf",
+    }
+    page.update()
 
-app(target=main)
+    page.clean()
+    page.add(create_ui(page))
+
+    
+app(target=main, assets_dir="assets")
 
