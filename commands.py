9c45a6f782eceef049c7d60cbd11255bb5062be7
Ileryadeh <ipdy@up.edu.ph>
2025-03-04 00:43:51 +0800

    Update commands.py
    added new function for profile page naviagtion

--- commands.py
+++ commands.py
@@ -1,3 +1,7 @@
+
+from profile_page import create_profile_ui
+
+#first interface connection to profile page
 def emergency_button(e):
     print("Emergency Button!")
 
@@ -8,7 +12,21 @@ def home_button(e):
     print("Home Button")
 
 def profile(e):
-    print("Profile")
+    e.page.clean() 
+    e.page.add(create_profile_ui(e.page))  
+    e.page.update()
 
 def notif(e):
     print("Notification")
+
+def medical(e):
+    print("Medical and Mobility Information")
+
+def medical_history(e):
+    print("Medical History")
+
+def legal_docu(e):
+    print("Legal & Documentation")
+
+def edit_profile(e):
+    print("Edit Profile")
